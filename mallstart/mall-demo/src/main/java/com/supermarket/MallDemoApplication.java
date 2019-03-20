1cffa00aaa60e90895b0ba783bd1947f2803d9ec
chenyichang <chenyichang_fh@12530.com>
2019-03-20 18:21:09 +0800

    初次提交demo

---
+++ mallstart/mall-demo/src/main/java/com/supermarket/MallDemoApplication.java
@@ -0,0 +1,14 @@
+package com.supermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @version 1.0 created by chenyichang_fh on 2019/3/20 18:00
+ */
+@SpringBootApplication
+public class MallDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallDemoApplication.class);
+    }
+}
---
+++ mallstart/mall-demo/src/main/java/com/supermarket/contoller/DemoController.java
@@ -0,0 +1,17 @@
+package com.supermarket.contoller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @version 1.0 created by chenyichang_fh on 2019/3/20 18:17
+ */
+@RestController
+public class DemoController {
+
+    @RequestMapping("/demo")
+    public String demo() {
+        return "hello,world";
+    }
+
+}
